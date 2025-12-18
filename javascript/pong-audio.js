b52b1b3165ae1b112ee917518f234e3157380838
Tobi Lawrence <telpioid@gmail.com>
2025-12-18 12:44:54 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,10 +52,10 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("silence.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("aqua_mixdown.mp3");
+export var ambientSound = new soundFile("aquamixdown.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = 12; //turn down volume
+ambientSound.player.volume.value = -20; //turn down volume
 
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
